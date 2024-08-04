5b36796aaf3c8ff59ddf9a860c06b9db1065cf66
Arham Hamza <arhamhamza05@gmail.com>
2024-08-05 01:08:28 +0500

    first commit

--- src/app/createMeme/page.js
+++ src/app/createMeme/page.js
@@ -1,29 +1,30 @@
 "use client"
 
-import React , {useState} from 'react'
-import './card.css'
+import React, { useState } from 'react';
+import './card.css';
 
-function createMeme({searchParams}) {
-  console.log(searchParams)
- const [text1, setText1] = useState("");
- const [text2, setText2] = useState("");
- const [fetchApi, setFetchApi] = useState(null);
- const genrateImg = () => {
-   if (!text1 || !text2) {
-     return alert("enter text");
-   }
-   fetch(
-     `https://api.imgflip.com/caption_image?template_id=${searchParams.id}&username=mrjunaid&password=junaid123&text0=${text1}&text1=${text2}`
-   )
-     .then((res) => res.json())
-     .then((response) => setFetchApi(response.data.url ));
- };
+function CreateMeme({ searchParams }) {
+  console.log(searchParams);
+  const [text1, setText1] = useState("");
+  const [text2, setText2] = useState("");
+  const [fetchApi, setFetchApi] = useState(null);
+
+  const generateImg = () => {
+    if (!text1 || !text2) {
+      return alert("enter text");
+    }
+    fetch(
+      `https://api.imgflip.com/caption_image?template_id=${searchParams.id}&username=mrjunaid&password=junaid123&text0=${text1}&text1=${text2}`
+    )
+      .then((res) => res.json())
+      .then((response) => setFetchApi(response.data.url));
+  };
 
   return (
     <div className="containerMeme container-fluid h-full">
       {!fetchApi ? (
         <div className='main-card'>
-          <img src={searchParams.imgUrl} width={300} />
+          <img src={searchParams.imgUrl} width={300} alt="Meme Template" />
           <div className="inputField">
             <input
               type="text"
@@ -31,24 +32,22 @@ function createMeme({searchParams}) {
               onChange={(e) => setText1(e.target.value)}
             />
             <input
-            className='input2'
+              className='input2'
               type="text"
               placeholder="enter text2"
               onChange={(e) => setText2(e.target.value)}
             />
           </div>
           <div className="btnDev">
-            <button className="gen-btn" onClick={genrateImg}>
-              Genrate Meme
+            <button className="gen-btn" onClick={generateImg}>
+              Generate Meme
             </button>
           </div>
         </div>
       ) : (
         <>
           <div className="user-Img">
-            <div className="">
-            </div>
-            <img src={fetchApi} />
+            <img src={fetchApi} alt="Generated Meme" />
           </div>
         </>
       )}
@@ -56,4 +55,4 @@ function createMeme({searchParams}) {
   );
 }
 
-export default createMeme
+export default CreateMeme;
